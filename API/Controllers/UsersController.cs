56ba2eeb40a71f29f1c17fd55b5a3f66924c3e4f
Paul Park <haejp1982@gmail.com>
2021-08-30 22:07:54 -0400

    remove period

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -22,7 +22,7 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            return await _context.Users.ToListAsync().;
+            return await _context.Users.ToListAsync();
         }
 
         [HttpGet("{id}")]
